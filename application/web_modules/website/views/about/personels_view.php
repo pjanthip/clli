32fb1e34c52e267910a4a0b4b3d31040831b90d9
Pranithan Janthip <p.janthip@gmail.com>
2023-08-08 11:19:50 +0700

    personels update

--- application/web_modules/website/views/about/personels_view.php
+++ application/web_modules/website/views/about/personels_view.php
@@ -35,7 +35,210 @@
     <ol class="flex-control-nav flex-control-paging"></ol></div>
 </div>
 
-<ul class="testimonials-grid grid-1 grid-md-2 grid-lg-3">
+<div class="fslider testimonial testimonial-full bottommargin" data-animation="fade" data-arrows="false">
+    <div class="text-end">
+        <span class="font-sarabun font-10 text-secondary"><i class="fa-solid fa-star"></i> ฝ่ายงานบริหารทั่วไป</span><br/>
+        <span class="font-sarabun text-secondary fst-italic" style="font-size:12px;">Office</span>
+    </div>
+    <div class="flexslider" style="height: 182px;">
+        <div class="slider-wrap">
+            <div class="slide flex-active-slide" style="width: 100%; float: left; margin-right: -100%; position: relative; opacity: 1; display: block; z-index: 2;">
+                <div class="testi-image">
+                    <a href="<?php echo base_url('assets/images/personnels/04.jpg')?>" data-lightbox="image"><img src="<?php echo base_url('assets/images/personnels/04.jpg')?>" alt="นางสาววิไลพร ริ้วสุวรรณ" draggable="false"></a>
+                </div>
+                <div class="testi-content">
+                    <div class="testi-meta non-meta">นางสาววิไลพร ริ้วสุวรรณ<span>เจ้าหน้าที่บริหารงานทั่วไป ชำนาญการ<br>หัวหน้าสำนักงาน</span></div>
+                </div>
+            </div>
+        </div>
+    <ol class="flex-control-nav flex-control-paging"></ol></div>
+    <ul class="testimonials-grid grid-1 grid-md-2 grid-lg-3">
+        <li class="grid-item">
+            <div class="testimonial">
+                <div class="testi-image">
+                    <a href="<?php echo base_url('assets/images/personnels/10.jpg')?>" data-lightbox="image">
+                    <img src="<?php echo base_url('assets/images/personnels/10.jpg')?>" alt="นายวินท์ปวัฒน์ พันธ์ด้วง"></a>
+                </div>
+                <div class="testi-content">
+                    <div class="testi-meta">นายวินท์ปวัฒน์ พันธ์ด้วง<span>เจ้าหน้าที่บริหารงานทั่วไป</span></div>
+                </div>
+            </div>
+        </li>
+        <li class="grid-item">
+            <div class="testimonial">
+                <div class="testi-image">
+                    <a href="<?php echo base_url('assets/images/personnels/17.jpg')?>" data-lightbox="image">
+                    <img src="<?php echo base_url('assets/images/personnels/17.jpg')?>" alt="นางบัญชา ตรูหม่อง"></a>
+                </div>
+                <div class="testi-content">
+                    <div class="testi-meta">นางบัญชา ตรูหม่อง<span>เจ้าหน้าที่บริการ</span></div>
+                </div>
+            </div>
+        </li>
+    </ul>
+</div>
+
+<div class="fslider testimonial testimonial-full bottommargin" data-animation="fade" data-arrows="false">
+    <div class="text-end">
+        <span class="font-sarabun font-10 text-secondary"><i class="fa-solid fa-star"></i> ฝ่ายงานคลังหน่วยกิต</span><br/>
+        <span class="font-sarabun text-secondary fst-italic" style="font-size:12px;">Creditbank : CB</span>
+    </div>
+    <div class="flexslider" style="height: 182px;">
+        <div class="slider-wrap">
+            <div class="slide flex-active-slide" style="width: 100%; float: left; margin-right: -100%; position: relative; opacity: 1; display: block; z-index: 2;">
+                <div class="testi-image">
+                    <a href="<?php echo base_url('assets/images/personnels/03.jpg')?>" data-lightbox="image"><img src="<?php echo base_url('assets/images/personnels/03.jpg')?>" alt="นางสาวดวงรัตน์ อินพรม" draggable="false"></a>
+                </div>
+                <div class="testi-content">
+                    <div class="testi-meta non-meta">นางสาวดวงรัตน์ อินพรม<span>นักวิชาการศึกษา<br>หัวหน้าฝ่ายงาน</span></div>
+                </div>
+            </div>
+        </div>
+    <ol class="flex-control-nav flex-control-paging"></ol></div>
+    <ul class="testimonials-grid grid-1 grid-md-2 grid-lg-3">
+        <li class="grid-item">
+            <div class="testimonial">
+                <div class="testi-image">
+                    <a href="<?php echo base_url('assets/images/personnels/05.jpg')?>" data-lightbox="image">
+                    <img src="<?php echo base_url('assets/images/personnels/05.jpg')?>" alt="นางสาวปรียะดา จอมแสง"></a>
+                </div>
+                <div class="testi-content">
+                    <div class="testi-meta">นางสาวปรียะดา จอมแสง<span>นักวิชาการศึกษา</span></div>
+                </div>
+            </div>
+        </li>
+        <li class="grid-item">
+            <div class="testimonial">
+                <div class="testi-image">
+                    <a href="<?php echo base_url('assets/images/personnels/07.jpg')?>" data-lightbox="image">
+                    <img src="<?php echo base_url('assets/images/personnels/07.jpg')?>" alt="นางพิชญา ศราวณะ"></a>
+                </div>
+                <div class="testi-content">
+                    <div class="testi-meta">นางพิชญา ศราวณะ<span>นักวิชาการศึกษา</span></div>
+                </div>
+            </div>
+        </li>
+        <li class="grid-item">
+            <div class="testimonial">
+                <div class="testi-image">
+                    <a href="<?php echo base_url('assets/images/personnels/12.jpg')?>" data-lightbox="image">
+                    <img src="<?php echo base_url('assets/images/personnels/12.jpg')?>" alt="นายนิยม ติยานันท์"></a>
+                </div>
+                <div class="testi-content">
+                    <div class="testi-meta">นายนิยม ติยานันท์<span>นักวิชาการช่างศิลป์</span></div>
+                </div>
+            </div>
+        </li>
+        <li class="grid-item">
+            <div class="testimonial">
+                <div class="testi-image">
+                    <a href="<?php echo base_url('assets/images/personnels/13.jpg')?>" data-lightbox="image">
+                    <img src="<?php echo base_url('assets/images/personnels/13.jpg')?>" alt="นายกรัณย์ กำสมุทร"></a>
+                </div>
+                <div class="testi-content">
+                    <div class="testi-meta">นายกรัณย์ กำสมุทร<span>นักวิชาการช่างศิลป์</span></div>
+                </div>
+            </div>
+        </li>
+        <li class="grid-item">
+            <div class="testimonial">
+                <div class="testi-image">
+                    <a href="<?php echo base_url('assets/images/personnels/14.jpg')?>" data-lightbox="image">
+                    <img src="<?php echo base_url('assets/images/personnels/14.jpg')?>" alt="นางสาวปรียากร เชียงพันธ์"></a>
+                </div>
+                <div class="testi-content">
+                    <div class="testi-meta">นางสาวปรียากร เชียงพันธ์<span>นักวิชาการช่างศิลป์</span></div>
+                </div>
+            </div>
+        </li>
+        <li class="grid-item">
+            <div class="testimonial">
+                <div class="testi-image">
+                    <a href="<?php echo base_url('assets/images/personnels/15.jpg')?>" data-lightbox="image">
+                    <img src="<?php echo base_url('assets/images/personnels/15.jpg')?>" alt="นางกชกร จี้อาทิตย์"></a>
+                </div>
+                <div class="testi-content">
+                    <div class="testi-meta">นางกชกร จี้อาทิตย์<span>นักวิชาการช่างศิลป์</span></div>
+                </div>
+            </div>
+        </li>
+        <li class="grid-item">
+            <div class="testimonial">
+                <div class="testi-image">
+                    <a href="<?php echo base_url('assets/images/personnels/16.jpg')?>" data-lightbox="image">
+                    <img src="<?php echo base_url('assets/images/personnels/16.jpg')?>" alt="นางสาวเบญญาดา สุคนธ์เขตร์"></a>
+                </div>
+                <div class="testi-content">
+                    <div class="testi-meta">นางสาวเบญญาดา สุคนธ์เขตร์<span>นักวิชาการช่างศิลป์</span></div>
+                </div>
+            </div>
+        </li>
+    </ul>
+</div>
+
+<div class="fslider testimonial testimonial-full bottommargin" data-animation="fade" data-arrows="false">
+    <div class="text-end">
+        <span class="font-sarabun font-10 text-secondary"><i class="fa-solid fa-star"></i> ฝ่ายงานบูรณาการเรียนรู้กับการทำงาน</span><br/>
+        <span class="font-sarabun text-secondary fst-italic" style="font-size:12px;">Cooperative and Work Integrated Education : CWIE</span>
+    </div>
+    <div class="flexslider" style="height: 182px;">
+        <div class="slider-wrap">
+            <div class="slide flex-active-slide" style="width: 100%; float: left; margin-right: -100%; position: relative; opacity: 1; display: block; z-index: 2;">
+                <div class="testi-image">
+                    <a href="<?php echo base_url('assets/images/personnels/08.jpg')?>" data-lightbox="image"><img src="<?php echo base_url('assets/images/personnels/08.jpg')?>" alt="นายยุทธภูมิ บัวใหญ่" draggable="false"></a>
+                </div>
+                <div class="testi-content">
+                    <div class="testi-meta non-meta">นายยุทธภูมิ บัวใหญ่<span>นักวิชาการศึกษา<br>หัวหน้าฝ่านงาน</span></div>
+                </div>
+            </div>
+        </div>
+    <ol class="flex-control-nav flex-control-paging"></ol></div>
+</div>
+
+<div class="fslider testimonial testimonial-full bottommargin" data-animation="fade" data-arrows="false">
+    <div class="text-end">
+        <span class="font-sarabun font-10 text-secondary"><i class="fa-solid fa-star"></i> ฝ่ายงานเทคโนโลยีสารสนเทศ</span><br/>
+        <span class="font-sarabun text-secondary fst-italic" style="font-size:12px;">Information Technology : IT</span>
+    </div>
+    <div class="flexslider" style="height: 182px;">
+        <div class="slider-wrap">
+            <div class="slide flex-active-slide" style="width: 100%; float: left; margin-right: -100%; position: relative; opacity: 1; display: block; z-index: 2;">
+                <div class="testi-image">
+                    <a href="<?php echo base_url('assets/images/personnels/11.jpg')?>" data-lightbox="image"><img src="<?php echo base_url('assets/images/personnels/11.jpg')?>" alt="นายปณิธาน จันทร์ทิพย์" draggable="false"></a>
+                </div>
+                <div class="testi-content">
+                    <div class="testi-meta non-meta">นายปณิธาน จันทร์ทิพย์<span>นักวิชาการคอมพิวเตอร์<br>หัวหน้าฝ่านงาน</span></div>
+                </div>
+            </div>
+        </div>
+    <ol class="flex-control-nav flex-control-paging"></ol></div>
+    <ul class="testimonials-grid grid-1 grid-md-2 grid-lg-3">
+        <li class="grid-item">
+            <div class="testimonial">
+                <div class="testi-image">
+                    <a href="<?php echo base_url('assets/images/personnels/19.jpg')?>" data-lightbox="image">
+                    <img src="<?php echo base_url('assets/images/personnels/19.jpg')?>" alt="นายธราธร ทิพยโสตถิ"></a>
+                </div>
+                <div class="testi-content">
+                    <div class="testi-meta">นายธราธร ทิพยโสตถิ<span>นักวิชาการคอมพิวเตอร์</span></div>
+                </div>
+            </div>
+        </li>
+        <li class="grid-item">
+            <div class="testimonial">
+                <div class="testi-image">
+                    <a href="<?php echo base_url('assets/images/personnels/18.jpg')?>" data-lightbox="image">
+                    <img src="<?php echo base_url('assets/images/personnels/18.jpg')?>" alt="นายปฏิภาณ สุขมาก"></a>
+                </div>
+                <div class="testi-content">
+                    <div class="testi-meta">นายปฏิภาณ สุขมาก<span>นักวิชาการคอมพิวเตอร์</span></div>
+                </div>
+            </div>
+        </li>
+    </ul>
+</div>
+
+<!-- <ul class="testimonials-grid grid-1 grid-md-2 grid-lg-3">
     <li class="grid-item">
         <div class="testimonial">
             <div class="testi-image">
@@ -44,17 +247,6 @@
             </div>
             <div class="testi-content">
                 <div class="testi-meta">นางสาวดวงรัตน์ อินพรม<span>นักวิชาการศึกษา</span></div>
-            </div>
-        </div>
-    </li>
-    <li class="grid-item">
-        <div class="testimonial">
-            <div class="testi-image">
-                <a href="<?php echo base_url('assets/images/personnels/04.jpg')?>" data-lightbox="image">
-                <img src="<?php echo base_url('assets/images/personnels/04.jpg')?>" alt="นางสาววิไลพร ริ้วสุวรรณ"></a>
-            </div>
-            <div class="testi-content">
-                <div class="testi-meta">นางสาววิไลพร ริ้วสุวรรณ<span>เจ้าหน้าที่บริหารงานทั่วไป ชำนาญการ</span></div>
             </div>
         </div>
     </li>
@@ -88,17 +280,6 @@
             </div>
             <div class="testi-content">
                 <div class="testi-meta">นายยุทธภูมิ บัวใหญ่<span>นักวิชาการศึกษา</span></div>
-            </div>
-        </div>
-    </li>
-    <li class="grid-item">
-        <div class="testimonial">
-            <div class="testi-image">
-                <a href="<?php echo base_url('assets/images/personnels/09.jpg')?>" data-lightbox="image">
-                <img src="<?php echo base_url('assets/images/personnels/09.jpg')?>" alt="นางสาวปราณีต ปงกันคำ"></a>
-            </div>
-            <div class="testi-content">
-                <div class="testi-meta">นางสาวปราณีต ปงกันคำ<span>เจ้าหน้าที่บริหารงานทั่วไป</span></div>
             </div>
         </div>
     </li>
@@ -212,7 +393,7 @@
             </div>
         </div>
     </li>
-</ul>
+</ul> -->
 
 <div class="line"></div>
 <style>
